29fe6e2a51c0612b0e17b646476b426cec5dc87a
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2024-03-25 16:22:46 +0100

    Update ITIL.py

--- ITIL.py
+++ ITIL.py
@@ -22,7 +22,8 @@ corrette = 0
 
 # Per ogni riga del dataframe, visualizza la domanda e le opzioni di risposta
 for i, row in test.iterrows():
-    st.write(f'Domanda {i + 1}):\n{row["domanda"]}')
+    st.write(f'Domanda {i + 1}):')
+    st.write(f'{row["domanda"]}')
     answer = row["errate"] + row["giusta"]  # Aggiungi la risposta giusta alla lista delle risposte errate
     random.shuffle(answer)
     for j, a in enumerate(answer):
