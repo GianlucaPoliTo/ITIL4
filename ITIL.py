a6c689a2a2207f0943fd903a1add82a5699bbb49
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2024-03-25 17:15:25 +0100

    Update ITIL.py

--- ITIL.py
+++ ITIL.py
@@ -6,7 +6,7 @@ st.title("Streamlit Quiz App for ITIL4 Exam")
 # Funzione per ottenere l'input dell'utente come un intero compreso tra 0 e 3
 def get_integer_input(prompt, key):
     with st.form("my_form"):
-        value = st.number_input("Insert number answer 0 -3", min_value=0, max_value=3, value=None, key=key)
+        value = st.number_input("Insert answer 0 - 3", min_value=0, max_value=3, value=None, key=key)
         submitted = st.form_submit_button("Submit")
     return value
 
