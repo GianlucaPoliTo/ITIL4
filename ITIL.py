c5c89624661e3ccb834e9b24abcdfbad4fb1398d
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2024-03-25 18:19:04 +0100

    Update ITIL.py

--- ITIL.py
+++ ITIL.py
@@ -5,8 +5,8 @@ import time
 st.title("Streamlit Quiz App for ITIL4 Exam")
 # Funzione per ottenere l'input dell'utente come un intero compreso tra 0 e 3
 
-def get_integer_input(prompt, key):
-    value = st.number_input("Insert answer 0 - 3", min_value=0, max_value=3, value=None, key=key)
+def get_integer_input(prompt, ):
+    value = st.number_input("Insert answer 0 - 3", min_value=0, max_value=3, value=None)
     return value
 
 # Carica il dataframe con le domande e le risposte
@@ -17,7 +17,7 @@ sbagliate = 0
 answer = []
 scelta = None
 # Per ogni riga del dataframe, visualizza la domanda e le opzioni di risposta
-st.write(f'#### Question {i}):')
+st.write(f'#### Question:')
 st.write(f'###### {row["domanda"]}')
 ## Aggiungi la risposta giusta alla lista delle risposte errate
 row["giusta"] = eval(row["giusta"])
@@ -27,7 +27,7 @@ random.shuffle(answer)
 for j, a in enumerate(answer):
     st.write(f"\t{j}. {a}\n")
     # Ottieni la risposta dall'utente
-scelta = get_integer_input("Inserisci risposta 0 - 3:", key=i)
+scelta = get_integer_input("Inserisci risposta 0 - 3:")
     # Controlla se la risposta è corretta
 if answer[scelta] == row["giusta"][0]:
     corrette += 1
