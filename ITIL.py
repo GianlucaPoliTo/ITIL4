d73b5f1a2ab8eb4b400223bc76530f12902010d3
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2024-03-25 18:18:27 +0100

    Update ITIL.py

--- ITIL.py
+++ ITIL.py
@@ -4,41 +4,36 @@ import random
 import time
 st.title("Streamlit Quiz App for ITIL4 Exam")
 # Funzione per ottenere l'input dell'utente come un intero compreso tra 0 e 3
+
 def get_integer_input(prompt, key):
     value = st.number_input("Insert answer 0 - 3", min_value=0, max_value=3, value=None, key=key)
     return value
-def score(lt):
-    st.write(f"Your final score is {corrette}/{lt}")
-    
+
 # Carica il dataframe con le domande e le risposte
 df_test = pd.read_csv('ITIL4_Exam.csv')
-n_domande=15
-test = df_test.sample(n_domande).reset_index(drop=True)
-test.index=test.index+1
 # Inizializza il punteggio corretto
 corrette = 0
+sbagliate = 0
 answer = []
 scelta = None
 # Per ogni riga del dataframe, visualizza la domanda e le opzioni di risposta
-with st.form("general_form", clear_on_submit=False):
-    for i, row in test.iterrows():
-        st.write(f'#### Question {i}):')
-        st.write(f'###### {row["domanda"]}')
-        ## Aggiungi la risposta giusta alla lista delle risposte errate
-        row["giusta"] = eval(row["giusta"])
-        row["errate"] = eval(row["errate"])
-        answer = row["errate"] + row["giusta"]
-        random.shuffle(answer)
-        for j, a in enumerate(answer):
-            st.write(f"\t{j}. {a}\n")
-            # Ottieni la risposta dall'utente
-        scelta = get_integer_input("Inserisci risposta 0 - 3:", key=i)
-            # Controlla se la risposta è corretta
-            #if answer[scelta] == row["giusta"][0]:
-            #    corrette += 1
-            #else:
-            #    st.write(f"Errato, risposta corretta: {row['giusta'][0]}")
+st.write(f'#### Question {i}):')
+st.write(f'###### {row["domanda"]}')
+## Aggiungi la risposta giusta alla lista delle risposte errate
+row["giusta"] = eval(row["giusta"])
+row["errate"] = eval(row["errate"])
+answer = row["errate"] + row["giusta"]
+random.shuffle(answer)
+for j, a in enumerate(answer):
+    st.write(f"\t{j}. {a}\n")
+    # Ottieni la risposta dall'utente
+scelta = get_integer_input("Inserisci risposta 0 - 3:", key=i)
+    # Controlla se la risposta è corretta
+if answer[scelta] == row["giusta"][0]:
+    corrette += 1
+else:
+    st.write(f"Errato, risposta corretta: {row['giusta'][0]}")
+    sbagliate +=1
     
-    st.form_submit_button(label="Submit", help=None, on_click=None, args=None, kwargs=None,)
 
 
