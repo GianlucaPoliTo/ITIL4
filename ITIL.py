285b6648bc9bec651eec34bed004a4f1e59978e0
Gianluca Perna <49552003+GianlucaPoliTo@users.noreply.github.com>
2024-03-25 16:38:02 +0100

    Update ITIL.py

--- ITIL.py
+++ ITIL.py
@@ -6,7 +6,7 @@ import random
 def get_integer_input(prompt, key):
     while True:
         try:
-            value = int(st.text_input(prompt, key=key+str(random.randint(0,100000))))
+            value = int(st.text_input(prompt, key=key+str(random.randint(0,100000)), default=0))
             if 0 <= value <= 3:
                 return value
             else:
